a5206d7af749e62efaf0a89b5bd2bf8fb801df80
trejas <thatch@tierrastudios.com>
2014-07-15 11:52:27 -0500

    Added gemspec changes

--- lib/yahoo_finance_historical.rb
+++ lib/yahoo_finance_historical.rb
@@ -156,6 +156,7 @@ end
 
   def self.read_historical(symbol, start_date, end_date, options)
      url = "http://ichart.yahoo.com/table.csv?s=#{URI.escape(symbol)}&d=#{end_date.month-1}&e=#{end_date.day}&f=#{end_date.year}&g=#{HISTORICAL_MODES[options[:period]]}&a=#{start_date.month-1}&b=#{start_date.day}&c=#{start_date.year}&ignore=.csv"
+     puts url
      conn = open(url)
      cols =
        if options[:period] == :dividends_only
